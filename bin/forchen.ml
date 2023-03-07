209650e94c5283cff7ddce8b5e00f7b4ed95a20b
ArtichautCosmique <lelolartichaut@laposte.net>
2023-03-07 02:53:31 +0100

    add args to program

--- bin/forchen.ml
+++ bin/forchen.ml
@@ -1,6 +1,22 @@
 module ANSI_Forchen = Forchen.Make (Forchen.Colors.ANSI) (Mirage_crypto_rng)
 
+let usage_msg = Printf.sprintf "%s [--mode MODE]" Sys.argv.(0)
+let mode = ref `Complex
+
+let speclist =
+  [
+    ( "--mode",
+      Arg.String
+        (function
+        | "nocolor" -> mode := `NoColor
+        | "simple" -> mode := `Simple
+        | "complex" -> mode := `Complex
+        | s -> failwith @@ "Unknown mode " ^ s),
+      "Select color mode (nocolor, simple or complex)" );
+  ]
+
 let () =
   Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna);
+  Arg.parse speclist (fun _ -> ()) usage_msg;
   print_endline
-  @@ ANSI_Forchen.fortune ~msg_list:Forchen.Fortune.all ~mode:`Complex ()
+  @@ ANSI_Forchen.fortune ~msg_list:Forchen.Fortune.all ~mode:!mode ()
